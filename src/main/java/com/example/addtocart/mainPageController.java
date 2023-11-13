0aabc4b23f8945fb79040cc108f5fc2c6ab13bb8
Jamey <jeremiah_sheehy1@my.vcccd.edu>
2023-11-13 15:08:59 -0800

    v0.0.5 Main page skeleton is done, on to the menu page

--- src/main/java/com/example/addtocart/mainPageController.java
+++ src/main/java/com/example/addtocart/mainPageController.java
@@ -1,4 +1,62 @@
 package com.example.addtocart;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class mainPageController {
+    @FXML
+    private Button b1g50off;
+
+    @FXML
+    private Button b2g1;
+
+    @FXML
+    private Button menu;
+
+    @FXML
+    private Button search;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private Button signout;
+
+    @FXML
+    void b1g50offButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void b2g1Button(MouseEvent event) {
+
+    }
+
+    @FXML
+    void menuButton(MouseEvent event) throws IOException {
+        Stage stage = (Stage) menu.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("menuPage.fxml"));
+        stage.setTitle("Menu");
+        stage.setScene(new Scene(root));
+
+
+    }
+
+    @FXML
+    void searchbarButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void signoutButton(MouseEvent event) {
+
+    }
 }
