526e3196d0b927257704582798da2fb4d43b6624
Jamey <jeremiah_sheehy1@my.vcccd.edu>
2023-11-15 00:06:17 -0800

    v0.0.6 Made some little additions to the menu page

--- src/main/java/com/example/addtocart/menuPageController.java
+++ src/main/java/com/example/addtocart/menuPageController.java
@@ -1,4 +1,25 @@
 package com.example.addtocart;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class menuPageController {
+    @FXML
+    private Button home;
+
+    @FXML
+    void homeButton(MouseEvent event) throws IOException {
+        Stage stage = (Stage) home.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
+        stage.setTitle("Menu");
+        stage.setScene(new Scene(root));
+
+    }
 }
